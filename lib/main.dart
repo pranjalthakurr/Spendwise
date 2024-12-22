73c42ab5931eb124d395ed19672048cac10d84be
Pranjal Thakur <91968368+pranjalthakurr@users.noreply.github.com>
2024-12-22 13:43:20 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'package:spendwise/widgets/expenses.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false, // Remove the debug banner
+      theme: ThemeData(
+        useMaterial3: true,
+        primaryColor: const Color.fromARGB(255, 48, 82, 71),
+        hintColor: const Color.fromARGB(255, 130, 227, 196),
+        scaffoldBackgroundColor: const Color(0xFFF9F8F7),
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(color: Color(0xFF3E3E3E)),
+        ),
+        cardTheme: const CardTheme().copyWith(
+          margin: const EdgeInsets.all(16),
+        ),
+      ),
+      home: const Expenses(),
+    ),
+  );
+}
